3a8f104bbdb7ea2ff4684251651f7ac19eb286c8
Hanake0 <bghaesee@outlook.com>
2022-01-15 12:11:52 -0300

    Feat(SearchAPI): Added total pages to API response

--- pages/api/chatbot/prices.tsx
+++ pages/api/chatbot/prices.tsx
@@ -17,6 +17,7 @@ export type PricesRequest = {
 export type PricesResponse = {
 	success: boolean,
 
+	total_pages: number,
 	needs_next_page_button: 0 | 1,
 	needs_previous_page_button: 0 | 1,
 }
@@ -41,13 +42,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 	});
 
 	const qtdRes = results.Avaliacoes[0].int_qtd_produto;
+	const totalPages = Math.ceil(qtdRes/Number(pr.items_per_page));
 
-	const needsNP = qtdRes > pr.page*Number(pr.items_per_page);
+	const needsNP = totalPages > pr.page;
 	const needsPP = pr.page > 1;
 
 	res.status(200).json({
 		success: true,
 
+		total_pages: totalPages,
 		needs_next_page_button: needsNP ? 1 : 0,
 		needs_previous_page_button: needsPP ? 1 : 0,
 	});
