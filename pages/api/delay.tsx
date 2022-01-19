a016176e18c24a9163bc9a1f29317599a92c69ac
Hanake0 <bghaesee@outlook.com>
2022-01-19 14:45:05 -0300

    Feat(DelayAPI): Added an api endpoint specifically to await a determined amount of time

---
+++ pages/api/delay.tsx
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import delay from "../../utils/delay";
+
+export type delayRequest = {
+	time: number,
+}
+
+export type delayResponse = {
+	success?: string,
+	error?: string,
+}
+
+export default async function handler (req: NextApiRequest, res: NextApiResponse<delayResponse>) {
+
+	const dr = req.body as delayRequest;
+
+	if (!Number.isInteger(dr.time))
+		return res.status(400).json({error: `${dr.time} is not an integer`});
+
+
+	await delay(dr.time);
+	res.status(200).json({success: `awaited ${dr.time}ms`});
+};
