084c82e2febf6c6f2d02db4c6742b67151c3a338
Hanake0 <bghaesee@outlook.com>
2022-01-21 17:00:52 -0300

    Feat(APIBusca): Refactored the API response and added images possibility

--- pages/api/delay.tsx
+++ pages/api/delay.tsx
@@ -17,7 +17,6 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
 	if (!Number.isInteger(dr.time))
 		return res.status(400).json({error: `${dr.time} is not an integer`});
 
-
 	await delay(dr.time);
 	res.status(200).json({success: `awaited ${dr.time}ms`});
 };
